8de8bb2f4152b1b2b2ec2e0b70c4a4f87f68f189
rajesh <rajesh.dnp01@gmail.com>
2021-06-24 21:03:05 +0530

    changed array vale

--- WebApplication2/Controllers/ValuesController.cs
+++ WebApplication2/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace WebApplication2.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "rajesh" };
+            return new string[] { "value1", "rajesh34" };
         }
 
         // GET api/values/5
